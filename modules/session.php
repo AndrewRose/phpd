736bacebdd45aaf4fe0e7def8ed68bc2feea5b76
rose.andrew <rose.andrew@9a2c095c-0741-0410-952d-7b146e57d71f>
2008-01-23 16:04:11 +0000

    ...

--- modules/session.php
+++ modules/session.php
@@ -4,6 +4,19 @@ class Phpd_Module_Session implements Phpd_Module
 {
 	public function init(Phpd_Child $o)
 	{
+	}
+
+	public function request(Phpd_Child $o)
+	{
+		/* this block should be in init.. but theres some wierd bug that causes it to fail */
+		if($o->reg->exists('session.database') && $o->reg->exists('database.'.$o->reg->get('session.database')))
+		{
+			$session = new Aplc_Session_Db;
+			$session->init($o->reg->getReference('database.'.$o->reg->get('session.database')));
+			$session->setHandlers();
+		}
+		/* */
+
 		if(!$o->reg->exists('session.name'))
 		{
 			$o->reg->set('session.name', 'PHPDSESSID');
@@ -23,8 +36,13 @@ class Phpd_Module_Session implements Phpd_Module
 		session_start();
 	}
 
-	public function deinit(Phpd_Child $o)
+	public function cleanup(Phpd_Child $o)
 	{
 		session_write_close();
+		unset($_SESSION);
+	}
+
+	public function deinit(Phpd_Child $o)
+	{
 	}
 }
