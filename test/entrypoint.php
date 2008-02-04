255f7ab7f21b8276a199111f047b2b47b063b026
rose.andrew <rose.andrew@9a2c095c-0741-0410-952d-7b146e57d71f>
2008-02-04 15:42:40 +0000

    ...

--- test/entrypoint.php
+++ test/entrypoint.php
@@ -3,12 +3,12 @@
 class test implements Phpd_Application
 {
 	public $counter = 0;
-	public $dbconn;
+	public $db;
 
         public function init(Phpd_Child $o)
 	{
-		//$this->dbcon = mysql_connect('localhost', 'root', '');
-		//mysql_select_db('iris', $this->dbcon);
+		$this->db = $o->reg->get('_phpd.module.Database.local.instance', TRUE);
+		$this->db->database('iris');
 	}
 
         public function response(Phpd_Child $o)
@@ -17,9 +17,9 @@ class test implements Phpd_Application
                 if(!isset($_SESSION['somevar'])) $_SESSION['somevar'] = rand(0, 1000000);
                 $ret = "Hello ".session_id()." from child: ".$o->pid.", the time and date is: ".date('d/m/Y H:i:s')." and I've been called: ".$this->counter." times.  Your random number is: ".$_SESSION['somevar']."\n";
 
-		//$result = mysql_query("select count(*) from iris_students", $this->dbcon);
-		//$row = mysql_fetch_array($result);
-		//$ret .= 'And the count in iris_students is: '.$row[0];
+		$result = $this->db->query("select count(*) as count from iris_students");
+		$row = $result->row();
+		$ret .= 'And the count in iris_students is: '.$row['count'];
 
                 return $ret;
 	}
@@ -31,6 +31,5 @@ class test implements Phpd_Application
 
         public function deinit(Phpd_Child $o)
 	{
-		//mysql_close($this->dbcon);
 	}
 }
