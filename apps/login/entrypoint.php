cb824fb0d55e6ce233790d910fc345d772d79d70
rose.andrew <rose.andrew@9a2c095c-0741-0410-952d-7b146e57d71f>
2008-03-05 17:19:26 +0000

    ...

---
+++ apps/login/entrypoint.php
@@ -0,0 +1,24 @@
+<?php
+
+class test implements Phpd_Application
+{
+        public function init(Phpd_Child $o)
+	{
+	}
+
+	public function request(Phpd_Child $o)
+	{
+	}
+
+        public function response(Phpd_Child $o)
+	{
+	}
+
+        public function cleanup(Phpd_Child $o)
+	{
+	}
+
+        public function deinit(Phpd_Child $o)
+	{
+	}
+}
