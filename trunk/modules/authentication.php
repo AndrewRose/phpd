48b89dd62496d7f035fbb5cc8ef19b35231a4375
rose.andrew <rose.andrew@9a2c095c-0741-0410-952d-7b146e57d71f>
2008-04-08 19:30:22 +0000

    Improved module loading, better loging, phpd_sendfile (a way to send files quickly) plus other tweaks here and there

--- trunk/modules/authentication.php
+++ trunk/modules/authentication.php
@@ -20,7 +20,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-class Phpd_Module_Auth implements Phpd_Module
+class Phpd_Module_Authentication implements Phpd_Module
 {
 	public $phpd;
 
